f95a511a0c08ba4c6e2325e1e537d4c9425fc7c5
daoistcoder <jeromeballena.dev@gmail.com>
2023-10-16 04:47:30 +0800

    ADD: models

---
+++ database/migrations/2023_10_15_203328_create_videos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->string('video_title', 100);
+            $table->text('video_url');
+            $table->text('video_description');
+            $table->foreignId('lesson_id')->constrained('lessons')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('videos');
+    }
+};
