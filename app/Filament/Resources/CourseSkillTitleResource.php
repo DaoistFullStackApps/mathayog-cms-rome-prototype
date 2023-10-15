eda5cfe21ef8f38fc69f0b387a3db61e458a1140
daoistcoder <jeromeballena.dev@gmail.com>
2023-10-16 06:07:25 +0800

    UPDATE: change labels

--- app/Filament/Resources/CourseSkillTitleResource.php
+++ app/Filament/Resources/CourseSkillTitleResource.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\CourseSkillTitleResource\Pages;
 use App\Filament\Resources\CourseSkillTitleResource\RelationManagers;
 use App\Models\CourseSkillTitle;
+use Closure;
 use Filament\Forms;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
@@ -18,6 +19,9 @@ use Illuminate\Database\Eloquent\SoftDeletingScope;
 class CourseSkillTitleResource extends Resource
 {
     protected static ?string $model = CourseSkillTitle::class;
+    protected static ?string $singularLabel = 'Course';
+    protected static ?string $pluralLabel = 'Courses';
+
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
@@ -26,14 +30,14 @@ class CourseSkillTitleResource extends Resource
         return $form
             ->schema([
                 Select::make('course_title')
-                ->options([
-                    'Counting to 1,000 Using Blocks' => 'Counting to 1,000 Using Blocks',
-                    'Counting to 1,000 Using Place Values' => 'Counting to 1,000 Using Place Values',
-                    'Illustrating Numbers to 1,000 Using Blocks' => 'Illustrating Numbers to 1,000 Using Blocks',
-                    'Comparing Numbers to 1,000 (Comparing Hundreds)' => 'Comparing Numbers to 1,000 (Comparing Hundreds)',
-                    'Ordering Numbers to 1,000 (Comparing Hundreds)' => 'Ordering Numbers to 1,000 (Comparing Hundreds)',
-                ])
-                ->required(),
+                    ->options([
+                        'Counting to 1,000 Using Blocks' => 'Counting to 1,000 Using Blocks',
+                        'Counting to 1,000 Using Place Values' => 'Counting to 1,000 Using Place Values',
+                        'Illustrating Numbers to 1,000 Using Blocks' => 'Illustrating Numbers to 1,000 Using Blocks',
+                        'Comparing Numbers to 1,000 (Comparing Hundreds)' => 'Comparing Numbers to 1,000 (Comparing Hundreds)',
+                        'Ordering Numbers to 1,000 (Comparing Hundreds)' => 'Ordering Numbers to 1,000 (Comparing Hundreds)',
+                    ])
+                    ->required(),
 
                 TextInput::make('skill_name')
                     ->required()
@@ -46,7 +50,7 @@ class CourseSkillTitleResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('course_title')
-                ->searchable(),
+                    ->searchable(),
                 Tables\Columns\TextColumn::make('skill_name')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('created_at')
@@ -71,11 +75,11 @@ class CourseSkillTitleResource extends Resource
                 ]),
             ]);
     }
-    
+
     public static function getPages(): array
     {
         return [
             'index' => Pages\ManageCourseSkillTitles::route('/'),
         ];
-    }    
+    }
 }
